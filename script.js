90270320de93f08a3c56418dd54d88612ccafe58
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2025-01-23 02:15:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-
 function setCookie(name, value, days) {
   const date = new Date();
   date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000); 
   document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/`;
 }
-
 
 function getCookie(name) {
   const cookies = document.cookie.split('; ');
@@ -15,11 +13,9 @@ function getCookie(name) {
   return null;
 }
 
-
 document.addEventListener('DOMContentLoaded', () => {
   const fontSizeInput = document.getElementById('fontsize');
   const fontColorInput = document.getElementById('fontcolor');
-  
 
   const savedFontSize = getCookie('fontSize');
   const savedFontColor = getCookie('fontColor');
@@ -34,17 +30,14 @@ document.addEventListener('DOMContentLoaded', () => {
     document.documentElement.style.setProperty('--fontcolor', savedFontColor);
   }
 
-
   document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault(); 
 
     const fontSize = fontSizeInput.value;
     const fontColor = fontColorInput.value;
 
-
     setCookie('fontSize', fontSize, 365); 
     setCookie('fontColor', fontColor, 365);
-
 
     document.documentElement.style.setProperty('--fontsize', `${fontSize}px`);
     document.documentElement.style.setProperty('--fontcolor', fontColor);
